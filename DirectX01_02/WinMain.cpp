904e905fd3ad97fe1bfa806c7551cc75d6574d57
moe3180 <moe.4kyoudai.tyoujo@gmail.com>
2020-05-17 16:37:44 +0900

    1-2

--- DirectX01_02/WinMain.cpp
+++ DirectX01_02/WinMain.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <vector>
@@ -14,6 +15,57 @@ ID3D12GraphicsCommandList* cmdList = nullptr;
 ID3D12CommandQueue* cmdQueue = nullptr;
 ID3D12DescriptorHeap* rtvHeaps = nullptr;
 
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	//メッセージで分岐
+	switch (msg)
+	{
+	case WM_DESTROY://ウィンドウが破棄された
+		PostQuitMessage(0);//OSに対して、アプリの終了を伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);//標準の処理を行う
+
+}
+
+//Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	//DirectX初期化処理ここから
+
+		//ウィンドウサイズ
+	const int window_width = 1280;//横幅
+	const int window_height = 720;//縦幅
+
+	WNDCLASSEX w{};//ウィンドウクラスの設定
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;//ウィンドウプロシージャを設定
+	w.lpszClassName = L"DirectXGame";//ウィンドウクラス名
+	w.hInstance = GetModuleHandle(nullptr);//ウィンドウハンドル
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);//カーソル指定
+
+	//ウィンドウクラスにOSを登録
+	RegisterClassEx(&w);
+	//ウィンドウサイズ(x座標,y座標,横幅,縦幅)
+	RECT wrc = { 0,0,window_width,window_height };
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);//自動でサイズ補正
+
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,//クラス名
+		L"DirectXGame",                      //タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,                 //標準的なウィンドウスタイル
+		CW_USEDEFAULT,                       //表示x座標(OSに任せる)
+		CW_USEDEFAULT,                       //表示y座標(OSに任せる)
+		wrc.right - wrc.left,                //ウィンドウ横幅
+		wrc.bottom - wrc.top,                //ウィンドウ縦幅
+		nullptr,                             //親ウィンドウハンドル
+		nullptr,                             //メニューハンドル
+		w.hInstance,                         //呼び出しアプリケーションハンドル
+		nullptr);                            //オプション
+
+	//ウィンドウ表示
+	ShowWindow(hwnd, SW_SHOW);
+
 //DXGIファクトリーの生成
 result = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
 //アダプターの列挙用
@@ -22,18 +74,18 @@ std::vector<IDXGIAdapter*>adapters;
 IDXGIAdapter* tmpAdapter = nullptr;
 
 for(int i=0;
-	dxgiFactory->EnumAdapters(i,&tmpAdapter)!=dxgi_ERROR_NOT_FOUND;i++)
+	dxgiFactory->EnumAdapters(i,&tmpAdapter)!=DXGI_ERROR_NOT_FOUND;i++)
 {
 	adapters.push_back(tmpAdapter);//動的配列に追加する
 }
 
-for (int i = 0; i < adapters.size(); I++) 
+for (int i = 0; i < adapters.size(); i++) 
 {
 	DXGI_ADAPTER_DESC adesc{};
 	adapters[i]->GetDesc(&adesc);//アダプターの情報を取得
 	std::wstring strDesc = adesc.Description;//アダプター名
 	//Microsoft Basic Render Driverを回避
-	if (stdDesc.find(L"Microsoft") == std::wstring::npos) 
+	if (strDesc.find(L"Microsoft") == std::wstring::npos) 
 	{
 		tmpAdapter = adapters[i];//採用
 		break;
@@ -61,4 +113,31 @@ for (int i = 0; i < _countof(levels); i++)
 		featureLevel = levels[i];
 		break;
 	}
+}
+	//初期化処理ここまで
+
+	MSG msg{};//メッセ―ジ
+
+	while (true) {
+		//メッセージがある？
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);//キー入力メッセージの処理
+			DispatchMessage(&msg);//プロs-じゃにメッセージを送る
+		}
+
+		//終了メッセージが来たらループを抜ける
+		if (msg.message == WM_QUIT) {
+			break;
+		}
+
+		//DirectX　毎フレーム処理ここから
+
+
+
+		//DirectX　毎フレーム処理ここまで
+
+	}
+
+	//ウィンドウクラスを登録解除
+	UnregisterClass(w.lpszClassName, w.hInstance);
 }
